de99f1fd3287ccfaca9812455491936c63bfb197
OpticalEngineering <optical_engineering@outlook.com>
2017-04-09 13:29:38 +0800

    Add files via upload

---
+++ Chapter/test1/src/test1/Circle.java
@@ -0,0 +1,14 @@
+package test1;
+
+public class Circle implements Shape2D {
+	
+	double radius;
+	public Circle(double r)
+	{
+		radius = r;
+	}
+	public double area() {
+		return(pi * radius *radius);
+	}
+
+}
