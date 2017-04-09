0c004356ce5e22a95fac9e13da0d6f3ef3ba202e
OpticalEngineering <optical_engineering@outlook.com>
2017-04-09 15:00:25 +0800

    Add files via upload

---
+++ Chapter04/Interface02/src/VariableTester.java
@@ -0,0 +1,13 @@
+
+public class VariableTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape2D var1,var2;
+		var1 = new Rectangle(5,6);
+		System.out.println("Area of var1 = " + var1.area());
+		var2 = new Circle(2.0);
+		System.out.println("Area of var2 = " + var2.area());
+	}
+
+}
